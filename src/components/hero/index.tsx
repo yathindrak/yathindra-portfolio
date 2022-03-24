7a0a4ed9489a35e8273fa13bd0657cdfc6375769
Yathindra <yathindrarawya123@gmail.com>
2022-03-25 00:02:56 +0530

    make hero-section responsive

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,9 +3,9 @@ import Box from "../common/box";
 import Row from "../common/row";
 
 const HeroImageSection = styled(Row)`
-  background-image: url(https://images.unsplash.com/photo-1500417148159-68083bd7333a);
+  background-image: url("profile-photo.png");
   background-position: center;
-  background-size: cover;
+  background-size: initial;
   background-repeat: no-repeat;
   // clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 25% 100%, 0% 50%);
   width: 22rem;
@@ -15,12 +15,20 @@ const HeroImageSection = styled(Row)`
   right: 20%;
   border-radius: 1rem;
   box-shadow: 0 10px 40px rgb(0 0 0 / 25%);
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const HeroDescriptionTitleContainer = styled(Box)`
   margin-top: 36vh;
   margin-left: 4rem;
   font-size: 4rem;
+  @media screen and (max-width: 768px) {
+    font-size: 3rem;
+    margin-left: 1rem;
+    margin-top: 3rem;
+  }
 `;
 
 const HeroDescriptionSubTitleContainer = styled(Box)`
@@ -28,12 +36,21 @@ const HeroDescriptionSubTitleContainer = styled(Box)`
   margin-top: 3rem;
   font-size: 1.5rem;
   color: #666464;
+  @media screen and (max-width: 768px) {
+    margin-left: 1rem;
+    margin-top: 3rem;
+  }
 `;
 
 const HeroDescriptionSection = styled(Box)`
   height: 100vh;
   width: 50%;
   font-weight: 600;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    text-align: center;
+    height: 50vh;
+  }
 `;
 
 const HeroDescriptionTitleText = styled.span`
@@ -44,6 +61,15 @@ const HeroDescriptionTitleSubText = styled.span`
   color: transparent;
   -webkit-text-stroke: 1px #000;
   display: block;
+`;
+
+const HeroImageContainer = styled(Box)`
+  height: 100vh;
+  width: 50%;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 interface NavBarProps {
@@ -70,9 +96,9 @@ export const HeroSection: React.FC<NavBarProps> = ({
           {subtitleText}
         </HeroDescriptionSubTitleContainer>
       </HeroDescriptionSection>
-      <div style={{ height: "100vh", width: "50%", position: "relative" }}>
+      <HeroImageContainer>
         <HeroImageSection />
-      </div>
+      </HeroImageContainer>
     </Row>
   );
 };
