a35979398919ba2c26d0176c38d7af39e8cfe121
Yathindra <yathindrarawya123@gmail.com>
2022-04-02 14:32:18 +0530

    add styles for portfolio

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Box from "../common/box";
 import Container from "../common/container";
 import Row from "../common/row";
-import mountains from "../../../public/abc.jpeg";
+import adapttextImage from "../../../public/adaptext.png";
 
 const PortfolioContainer = styled(Container)`
   display: flex;
@@ -14,6 +14,9 @@ const PortfolioMenuContainer = styled(Row)`
   justify-content: flex-end;
   width: 100%;
   margin-bottom: 3rem;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const PortfolioMenuItem = styled(Box)`
@@ -22,8 +25,22 @@ const PortfolioMenuItem = styled(Box)`
   cursor: pointer;
 `;
 
-const PortfolioCardsContainer = styled(Row)`
-  justify-content: space-between;
+const PortfolioMobileTitle = styled(Box)`
+  align-self: center;
+  font-size: 1.2rem;
+  font-weight: 500;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+interface PortfolioCardsContainerProps {
+  // space evenly when only two cards are available
+  readonly shouldSpaceEvenly: boolean;
+}
+
+const PortfolioCardsContainer = styled(Row)<PortfolioCardsContainerProps>`
+  justify-content: ${(props) => (props.shouldSpaceEvenly ? "space-evenly" : "space-between")};
   flex-wrap: wrap;
 `;
 
@@ -41,14 +58,23 @@ const PortfolioCardOverlayContainer = styled(Row)`
 `;
 
 const PortfolioCardContainer = styled(Row)`
-  width: 23%;
-  background-color: salmon;
-  margin-bottom: 1rem;
   position: relative;
+  cursor: pointer;
+  height: 17rem;
+  width: 25rem;
+  margin-bottom: 2.5rem;
   &:hover ${PortfolioCardOverlayContainer} {
     opacity: 1;
     color: #fff;
-  }
+  };
+  @media screen and (max-width: 992px) {
+    width: calc(50% - 1rem);
+    margin-bottom: 0;
+  };
+  @media screen and (max-width: 576px) {
+    width: 100%;
+    margin-bottom: 0;
+  };
 `;
 
 const PortfolioOverlayHeading = styled(Row)`
@@ -70,88 +96,29 @@ export const Portfolio: React.FC = ({}) => {
         <PortfolioMenuItem>Concept</PortfolioMenuItem>
         <PortfolioMenuItem>Development</PortfolioMenuItem>
       </PortfolioMenuContainer>
+      <PortfolioMobileTitle>My Portfolio</PortfolioMobileTitle>
 
-      <PortfolioCardsContainer>
-        <PortfolioCardContainer>
-          <Image
-            src={mountains}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Picture of the author"
-          />
-          <PortfolioCardOverlayContainer>
-            <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
-            <PortfolioOverlayDescription>
-              Description
-            </PortfolioOverlayDescription>
-          </PortfolioCardOverlayContainer>
-        </PortfolioCardContainer>
-
-        <PortfolioCardContainer>
-          <Image
-            src={mountains}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Picture of the author"
-          />
-          <PortfolioCardOverlayContainer>
-            <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
-            <PortfolioOverlayDescription>
-              Description
-            </PortfolioOverlayDescription>
-          </PortfolioCardOverlayContainer>
-        </PortfolioCardContainer>
-
-        <PortfolioCardContainer>
-          <Image
-            src={mountains}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Picture of the author"
-          />
-          <PortfolioCardOverlayContainer>
-            <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
-            <PortfolioOverlayDescription>
-              Description
-            </PortfolioOverlayDescription>
-          </PortfolioCardOverlayContainer>
-        </PortfolioCardContainer>
-
-        <PortfolioCardContainer>
-          <Image
-            src={mountains}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Picture of the author"
-          />
-          <PortfolioCardOverlayContainer>
-            <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
-            <PortfolioOverlayDescription>
-              Description
-            </PortfolioOverlayDescription>
-          </PortfolioCardOverlayContainer>
-        </PortfolioCardContainer>
-
-        <PortfolioCardContainer>
-          <Image
-            src={mountains}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Picture of the author"
-          />
-          <PortfolioCardOverlayContainer>
-            <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
-            <PortfolioOverlayDescription>
-              Description
-            </PortfolioOverlayDescription>
-          </PortfolioCardOverlayContainer>
-        </PortfolioCardContainer>
-        
+      <PortfolioCardsContainer shouldSpaceEvenly={true}>
+        {
+          [1,2].map((item, i)=> (
+            <PortfolioCardContainer key={i}>
+              <Image
+                  src={adapttextImage}
+                  layout="fill"
+                  width={200}
+                  objectFit="contain"
+                  placeholder="blur"
+                  alt="Picture of the author"
+                />
+                <PortfolioCardOverlayContainer>
+                  <PortfolioOverlayHeading>Title</PortfolioOverlayHeading>
+                  <PortfolioOverlayDescription>
+                    Description
+                  </PortfolioOverlayDescription>
+                </PortfolioCardOverlayContainer>
+            </PortfolioCardContainer>
+          ))
+        }
       </PortfolioCardsContainer>
     </PortfolioContainer>
   );
